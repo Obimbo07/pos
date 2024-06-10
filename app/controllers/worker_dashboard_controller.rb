cf87ed7957f3b2262967ebb7b5d5070af49e4a38
Obimbo07 <obimboausts@gmail.com>
2024-06-10 19:41:06 +0300

    Feature: ✔Booking complete

--- app/controllers/worker_dashboard_controller.rb
+++ app/controllers/worker_dashboard_controller.rb
@@ -2,7 +2,7 @@ class WorkerDashboardController < ApplicationController
     # before_action :authenticate_worker!
   
     def index
-      service = current_worker.services
+     
     end
   end
   
