0243628c8cac701ad8a2ca979b606a74bee5f056
Obimbo07 <obimboausts@gmail.com>
2024-06-20 23:20:53 +0300

    Feature: ✔ added commissions total to worker dashboard

--- app/controllers/worker_dashboard_controller.rb
+++ app/controllers/worker_dashboard_controller.rb
@@ -4,6 +4,7 @@ class WorkerDashboardController < ApplicationController
       @current_worker = current_worker
       @services = @current_worker.services
       @worker_commissions = @current_worker.worker_commissions
+      @total_commission = @worker_commissions.sum { |commission| commission.commission || 0}
     end
   end
   
