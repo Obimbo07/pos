0b02d1a30056ac7a01255c290570917d6c8869bf
Obimbo07 <obimboausts@gmail.com>
2024-06-26 14:15:59 +0300

    trial push

--- app/models/worker_commission.rb
+++ app/models/worker_commission.rb
@@ -1,4 +1,8 @@
 class WorkerCommission < ApplicationRecord
   belongs_to :worker
   belongs_to :booking_history
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["booking_history_id", "commission", "created_at", "id", "id_value", "timestamp", "updated_at", "worker_id"]
+  end
 end
