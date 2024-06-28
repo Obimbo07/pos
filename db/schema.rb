6746105f2ef481f9925c0580cd2e199a5b23b5f6
Obimbo07 <obimboausts@gmail.com>
2024-06-28 22:38:43 +0300

    Feature: 🤝 added is_paid and payment-method attributes to the booking_history

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_190804) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_28_191401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_20_190804) do
     t.integer "inventory_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_paid"
+    t.string "payment_method"
   end
 
   create_table "booking_histories_workers", id: false, force: :cascade do |t|
